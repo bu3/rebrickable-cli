832ed59fd42ce103c3444814c0c90c346ac94cbb
Fabio Mangione <fmangione@salesforce.com>
2023-02-28 11:16:32 +0000

    Add Auth data to Context - refactoring - share token and api into context

--- cmd/sets.go
+++ cmd/sets.go
@@ -4,19 +4,18 @@ import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var setNumber string
 
 func init() {
 	user.AddCommand(setsCmd)
-	setsCmd.AddCommand(getSets)
-	setsCmd.AddCommand(saveSets)
-	setsCmd.AddCommand(deleteSets)
+	setsCmd.AddCommand(getSetsCmd)
+	setsCmd.AddCommand(saveSetsCmd)
+	setsCmd.AddCommand(deleteSetsCmd)
 
-	saveSets.Flags().StringVarP(&setNumber, "set_num", "n", "", "Set numbers")
-	deleteSets.Flags().StringVarP(&setNumber, "set_num", "n", "", "Set numbers")
+	saveSetsCmd.Flags().StringVarP(&setNumber, "set_num", "n", "", "Set numbers")
+	deleteSetsCmd.Flags().StringVarP(&setNumber, "set_num", "n", "", "Set numbers")
 }
 
 var setsCmd = &cobra.Command{
@@ -27,102 +26,77 @@ var setsCmd = &cobra.Command{
 	},
 }
 
-var getSets = &cobra.Command{
+var getSetsCmd = &cobra.Command{
 	Use:   "get",
 	Short: "get",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		client := resty.New()
-		apiKey, authToken := Login(client)
-		//Get sets again
+		authToken := cmd.Context().Value(AuthToken).(string)
+		apiKey := cmd.Context().Value(ApiKey).(string)
 		GetUserSets(client, apiKey, authToken)
 		return nil
 	},
 }
 
-var deleteSets = &cobra.Command{
+var deleteSetsCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "delete",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		client := resty.New()
-		apiKey, authToken := Login(client)
+		authToken := cmd.Context().Value(AuthToken).(string)
+		apiKey := cmd.Context().Value(ApiKey).(string)
 		DeleteUserSet(client, apiKey, authToken)
 		return nil
 	},
 }
 
-var saveSets = &cobra.Command{
+var saveSetsCmd = &cobra.Command{
 	Use:   "set",
 	Short: "set",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		client := resty.New()
-		apiKey, authToken := Login(client)
+		authToken := cmd.Context().Value(AuthToken).(string)
+		apiKey := cmd.Context().Value(ApiKey).(string)
 		StoreUserSet(client, apiKey, authToken, setNumber)
-
 		return nil
 	},
 }
 
-func Login(client *resty.Client) (string, *AuthToken) {
-	username := os.Getenv("REBRICKABLE_USERNAME")
-	password := os.Getenv("REBRICKABLE_PASSWORD")
-	apiKey := os.Getenv("REBRICKABLE_API_KEY")
-	authToken := &AuthToken{}
-
-	resp, _ := client.R().
-		SetHeader("Content-Type", "application/x-www-form-urlencoded").
-		SetHeader("Authorization", fmt.Sprintf("key %s", apiKey)).
-		SetFormData(map[string]string{
-			"username": username,
-			"password": password,
-		}).
-		SetResult(authToken).
-		Post("https://rebrickable.com/api/v3/users/_token/")
-
-	if resp.StatusCode() == 200 {
-		fmt.Println("Login successful!!")
-	}
-	return apiKey, authToken
-}
-
-func DeleteUserSet(client *resty.Client, apiKey string, authToken *AuthToken) {
+func DeleteUserSet(client *resty.Client, apiKey string, authToken string) {
 	resp, _ := client.R().
 		SetHeader("Content-Type", "application/json").
 		SetHeader("Authorization", fmt.Sprintf("key %s", apiKey)).
-		Delete(fmt.Sprintf("https://rebrickable.com/api/v3/users/%s/sets/%s/", authToken.UserToken, setNumber))
+		Delete(fmt.Sprintf("https://rebrickable.com/api/v3/users/%s/sets/%s/", authToken, setNumber))
 
 	if resp.StatusCode() == 204 {
 		fmt.Println("Deleted set: 10276-1")
 	}
 }
 
-func StoreUserSet(client *resty.Client, apiKey string, authToken *AuthToken, setNumber string) {
+func StoreUserSet(client *resty.Client, apiKey string, authToken string, setNumber string) {
 	resp, _ := client.R().
 		SetHeader("Content-Type", "application/json").
 		SetHeader("Authorization", fmt.Sprintf("key %s", apiKey)).
 		SetBody(fmt.Sprintf(`{"set_num":  "%s","quantity": "1"}`, setNumber)).
-		Post(fmt.Sprintf("https://rebrickable.com/api/v3/users/%s/sets/", authToken.UserToken))
+		Post(fmt.Sprintf("https://rebrickable.com/api/v3/users/%s/sets/", authToken))
 
 	if resp.StatusCode() == 201 {
 		fmt.Println("Sets saved: ", resp)
 	}
 }
 
-func GetUserSets(client *resty.Client, apiKey string, authToken *AuthToken) *SetsResponse {
+func GetUserSets(client *resty.Client, apiKey string, authToken string) *SetsResponse {
 	setsResponse := &SetsResponse{}
 	resp, _ := client.R().
 		SetHeader("Content-Type", "application/json").
 		SetHeader("Authorization", fmt.Sprintf("key %s", apiKey)).
 		SetResult(setsResponse).
-		Get(fmt.Sprintf("https://rebrickable.com/api/v3/users/%s/sets", authToken.UserToken))
+		Get(fmt.Sprintf("https://rebrickable.com/api/v3/users/%s/sets", authToken))
 
 	if resp.StatusCode() == 200 {
 		fmt.Println("Sets Found: ", resp)
 	}
 	return setsResponse
-}
-
-type AuthToken struct {
-	UserToken string `json:"user_token"`
 }
 
 type SetsResponse struct {
--- cmd/user.go
+++ cmd/user.go
@@ -1,20 +1,63 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
+	"golang.org/x/net/context"
+	"os"
 )
 
 func init() {
 	rootCmd.AddCommand(user)
 }
 
+const (
+	ApiKey    = "api_key"
+	AuthToken = "auth_token"
+)
+
 var user = &cobra.Command{
 	Use:   "user",
 	Short: "user actions",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		//if err := someFunc(); err != nil {
-		//	return err
-		//}
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		client := resty.New()
+		authToken := login(client)
+		ctx := context.WithValue(cmd.Context(), AuthToken, authToken.UserToken)
+		ctx = context.WithValue(ctx, ApiKey, authToken.ApiKey)
+		cmd.SetContext(ctx)
 		return nil
 	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+func login(client *resty.Client) *authToken {
+	username := os.Getenv("REBRICKABLE_USERNAME")
+	password := os.Getenv("REBRICKABLE_PASSWORD")
+	apiKey := os.Getenv("REBRICKABLE_API_KEY")
+	authToken := &authToken{
+		ApiKey: apiKey,
+	}
+
+	resp, _ := client.R().
+		SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetHeader("Authorization", fmt.Sprintf("key %s", apiKey)).
+		SetFormData(map[string]string{
+			"username": username,
+			"password": password,
+		}).
+		SetResult(authToken).
+		Post("https://rebrickable.com/api/v3/users/_token/")
+
+	if resp.StatusCode() == 200 {
+		fmt.Println("Login successful!!")
+	}
+	return authToken
+}
+
+type authToken struct {
+	UserToken string `json:"user_token"`
+	ApiKey    string
 }
