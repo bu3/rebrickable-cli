dc45b80370de7ec76195e5847e9ff6b821a6c2b5
Fabio Mangione <fmangione@salesforce.com>
2024-06-11 17:08:44 +0100

    add test script

---
+++ test.sh
@@ -0,0 +1,6 @@
+# 10274
+target/rebrickable-cli user sets delete -n 10274-1
+target/rebrickable-cli user sets delete -n 10274
+target/rebrickable-cli user sets get
+target/rebrickable-cli user sets set -n 10274
+target/rebrickable-cli user sets get
