dc260c95251c8f7185494e13a71afc2fae9cc897
Fabio Mangione <fabio.mangione@gmail.com>
2026-05-27 16:21:14 +0100

    feat: add lego colors CLI commands

---
+++ cli/cmd/lego_colors.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var colorID string
+
+func init() {
+	legoColorsCommands()
+}
+
+func legoColorsCommands() {
+	legoCmd.AddCommand(legoColorsCmd)
+	legoColorsCmd.AddCommand(getLegoColorsCmd)
+	legoColorsCmd.AddCommand(getLegoColorCmd)
+
+	getLegoColorCmd.Flags().StringVarP(&colorID, "id", "i", "", "Color id")
+	_ = getLegoColorCmd.MarkFlagRequired("id")
+}
+
+var legoColorsCmd = &cobra.Command{
+	Use:   "colors",
+	Short: "LEGO catalog color actions",
+}
+
+var getLegoColorsCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list all colors",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client := newLegoAPIClient(cmd)
+		result, err := client.GetLegoColors()
+		if err != nil {
+			return err
+		}
+		return printJSON(result)
+	},
+}
+
+var getLegoColorCmd = &cobra.Command{
+	Use:   "get",
+	Short: "get a color by id",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client := newLegoAPIClient(cmd)
+		result, err := client.GetLegoColor(colorID)
+		if err != nil {
+			return err
+		}
+		return printJSON(result)
+	},
+}
