180527fed74648435626272880c1eca88707dea9
Fabio Mangione <fabio.mangione@gmail.com>
2026-06-15 19:26:41 +0100

    update script

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 set -euo pipefail
 
 bazel build //cli:cli --ui_event_filters=-info --noshow_progress 2>/dev/null
-bazel-bin/cli/cli_/cli "$@"
+
+# .bazel directory is a symlink configured in .bazelrc
+.bazel/bin/cli/cli_/cli "$@"
